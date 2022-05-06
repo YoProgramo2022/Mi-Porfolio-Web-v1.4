8fcc8c0262411d0f283bddb8c87ec7803f16b771
Nicolas Castellini <100251427+YoProgramo2022@users.noreply.github.com>
2022-05-05 23:34:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ const btnEnvio=document.getElementsByid("enviarcorreo");
 
 btnEnvio.addEventListener('click', function(e) {
     e.preventDefault();
-    const nombre=document.getElementById("nombre").value  ;
-    const apellido=document.getElementById("apellido").value;
-    const mensaje=document.getElementById("mensaje").value;
+    const nombre=document.getElementById('nombre').value  ;
+    const apellido=document.getElementById('apellido').value;
+    const mensaje=document.getElementById('mensaje').value;
     windows.location.href= `mailto: nicolasrcastellini@gmail.com subject=envioDesdeFormulario&body=nombre%3A${nombre}%0Aapellido%3A${apellido}%0Amensaje%3A${mensaje}`;
 })
